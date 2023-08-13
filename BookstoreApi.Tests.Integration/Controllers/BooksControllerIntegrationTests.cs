36db2194b20c66f8cb40cf93b5e4c2e6eb3f096b
Alex120gb <alexis.krtz120@gmail.com>
2023-08-13 09:15:42 +0300

    Testing projects (Integration & Unit tests)

---
+++ BookstoreApi.Tests.Integration/Controllers/BooksControllerIntegrationTests.cs
@@ -0,0 +1,166 @@
+﻿using BookstoreApi.Common;
+using BookstoreApi.ViewModels;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace BookstoreApi.Tests.Integration.Controllers
+{
+    [TestFixture]
+    public class BooksControllerIntegrationTests
+    {
+        private HttpClient _client;
+        private MockJwtTokenGenerator _jwtTokenGenerator;
+        private string _validJwtToken;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+
+            _client = appFactory.CreateClient();
+
+            _jwtTokenGenerator = new MockJwtTokenGenerator();
+            _validJwtToken = _jwtTokenGenerator.GenerateJwtToken("TestUser");
+        }
+
+        [Test]
+        public async Task Books_ReturnsSuccessStatusCode()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _validJwtToken);
+
+            // Act
+            var response = await _client.GetAsync("/BooksApi/GetAllBooks");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+
+            var books = await response.Content.ReadFromJsonAsync<List<GetUpdateBooksModel>>();
+            Assert.NotNull(books);
+        }
+
+        [Test]
+        public async Task Books_ReturnsFailUnauthorized()
+        {
+            // Arrange & Act
+            var response = await _client.GetAsync("/BooksApi/GetAllBooks");
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public async Task AddBooks_ReturnsCreatedStatusCode()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _validJwtToken);
+            // Remember to change
+            // the title for every new test run
+            var newBook = new AddBooksModel() 
+            { 
+                Title = "TestBook2-add-new-book", 
+                Author = "IntegrationTest-Add-2",
+                PublicationYear = "2023",
+                Isbn = "TEST-2023-integration"
+            };
+
+            // Act
+            var response = await _client.PostAsJsonAsync("/BooksApi/AddBook", newBook);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+
+            var createdBookResponse = await response.Content.ReadFromJsonAsync<Response<int>>();
+            Assert.NotNull(createdBookResponse);
+            Assert.IsTrue(createdBookResponse.IsSuccessful == true);
+        }
+
+        [Test]
+        public async Task AddBooks_ReturnsFailUnauthorized()
+        {
+            // Arrange
+            var newBook = new AddBooksModel();
+
+            // Act
+            var response = await _client.PostAsJsonAsync("/BooksApi/AddBook", newBook);
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public async Task UpdateBook_ReturnsUpdatedStatusCode()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _validJwtToken);
+            var updateBook = new GetUpdateBooksModel()
+            {
+                Id = 54,
+                Title = "TestBook - Testing Update Functionality",
+                Author = "IntegrationTest-2",
+                PublicationYear = "2023",
+                Isbn = "TEST-2023-integration"
+            };
+
+            // Act
+            var response = await _client.PutAsJsonAsync("/BooksApi/UpdateExistingBook", updateBook);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+
+            var updatedBookResponse = await response.Content.ReadFromJsonAsync<Response<int>>();
+            Assert.NotNull(updatedBookResponse);
+            Assert.IsTrue(updatedBookResponse.IsSuccessful == true);
+        }
+
+        [Test]
+        public async Task UpdateBook_ReturnsFailUnauthorized()
+        {
+            // Arrange
+            var updateBook = new GetUpdateBooksModel();
+
+            // Act
+            var response = await _client.PutAsJsonAsync("/BooksApi/UpdateExistingBook", updateBook);
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public async Task DeleteBook_ReturnsDeletedStatusCode()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _validJwtToken);
+            var deleteBook = 55;
+
+            // Act
+            var response = await _client.DeleteAsync("/BooksApi/DeleteExistingBook/" + deleteBook);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+
+            var deletedBookResponse = await response.Content.ReadFromJsonAsync<Response<int>>();
+            Assert.NotNull(deletedBookResponse);
+            Assert.IsTrue(deletedBookResponse.IsSuccessful == true);
+        }
+
+        [Test]
+        public async Task DeleteBook_ReturnsFailUnauthorized()
+        {
+            // Arrange
+            var deleteBook = 55;
+
+            // Act
+            var response = await _client.DeleteAsync("/BooksApi/DeleteExistingBook/" + deleteBook);
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+        }
+    }
+}
---
+++ BookstoreApi.Tests.Integration/SdkClients/BooksSdkClientIntegrationTest.cs
@@ -0,0 +1,64 @@
+﻿using BookstoreApi.Common;
+using BookstoreApi.ViewModels;
+using BookstoreSdk.Clients;
+using BookstoreSdk.Clients.Interface;
+using BookstoreSdk.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreApi.Tests.Integration.SdkClients
+{
+    [TestFixture]
+    public class BooksSdkClientIntegrationTest
+    {
+        private HttpClient _httpClient;
+        private ClientBookService _bookClientService;
+        private IBookClient _bookClient;
+        private MockJwtTokenGenerator _jwtTokenGenerator;
+        private string _validJwtToken;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _httpClient = new HttpClient();
+            //_httpClient.BaseAddress = new Uri("https://localhost:44388");
+
+            _jwtTokenGenerator = new MockJwtTokenGenerator();
+            _validJwtToken = _jwtTokenGenerator.GenerateJwtToken("TestUser");
+
+            _bookClient = new BookClient(new ClientBookService("http://localhost:5000"));
+        }
+
+        [Test]
+        public async Task Books_ReturnsSuccessStatusCode()
+        {
+            // Arrange
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _validJwtToken);
+
+            // Act
+            var response = await _bookClient.GetBooks(_validJwtToken);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.IsNotEmpty(response);
+        }
+
+        [Test]
+        public async Task Books_ReturnsFailUnauthorized()
+        {
+            // Arrange & Act
+            var response = await _bookClientService.GetBooks(_validJwtToken);
+
+            // Assert
+            Assert.IsNull(response);
+            Assert.IsEmpty(response);
+        }
+    }
+}
